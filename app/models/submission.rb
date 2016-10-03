422cf1ae293e29a42a19098b57f5308a00f32a10
Chris Foral <chris.foral@tjmartineaus-MacBook-Pro.local>
2016-10-03 15:37:39 -0500

    Defined Serializers, commented them out. DB table naming aliasing

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ApplicationRecord
+  self.table_name = 'app_bldr_submissions'
   belongs_to :form
   belongs_to :field
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,8 @@
 class Template < ApplicationRecord
+  self.table_name = 'app_bldr_templates'
   has_many :forms
   has_many :template_fields
   has_many :submissions, through: :forms
   
-  belongs_to :user, :foreign_key => "insert_by"
+  belongs_to :user, :foreign_key => "insert_by", :class_name => 'User'
 end
