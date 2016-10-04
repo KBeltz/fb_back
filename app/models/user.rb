f3fc64e9867dcd60fc819a391dc767e1dcff3285
KBeltz <beltz08@me.com>
2016-10-04 07:41:57 -0500

    Updated foreign key class references for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :templates, :foreign_key => "insert_by", :class_name => 'User'
-  has_many :forms, :foreign_key => "recipient_user_id", :class_name => 'User'
+  has_many :templates, :foreign_key => "insert_by", :class_name => 'Template'
+  has_many :forms, :foreign_key => "recipient_user_id", :class_name => 'Form'
   has_many :template_fields, through: :templates
   has_many :submissions, through: :forms
 end
