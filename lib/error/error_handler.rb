b776866c4eb9702ea43679c30abef383892c02f7
Chris Foral <chris.foral@atssecured.com>
2016-10-19 09:23:06 -0500

    Refactored error_handler to recover from various errors

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -3,8 +3,14 @@ module Error
     # self.included is invoked when the class that includes this module is run (http://apidock.com/ruby/Module/included)
     def self.included(clazz)
       clazz.class_eval do
+        rescue_from ActiveRecord::RecordNotFound do |e|
+          respond(:record_not_found, 404, e.to_s)
+        end
         rescue_from CustomError do |e|
-          respond(e.error, e.status, e.message)
+          respond(e.error, e.status, e.message.to_s)
+        end
+        rescue_from StandardError do |e|
+          respond(:standard_error, 500, e.to_s)
         end
       end
     end
@@ -13,7 +19,7 @@ module Error
 
     def respond(_error, _status, _message)
       json = Helpers::Render.json(_error, _status, _message)
-      render json: json
+      render json: json, status: _status
     end
   end
 end
