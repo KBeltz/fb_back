7de8d9bbce431e5be79ddd07bd60330d24dd4757
Chris Foral <chris.foral@atssecured.com>
2016-10-19 15:24:08 -0500

    Made more explicit variable name in error_handler

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -3,17 +3,16 @@ module Error
     # self.included is invoked when the class that includes this module is run (http://apidock.com/ruby/Module/included)
     def self.included(clazz)
       clazz.class_eval do
-        rescue_from ActiveRecord::RecordNotFound do |e|
-          respond(:record_not_found, 404, e.to_s)
+        rescue_from ActiveRecord::RecordNotFound do |exception|
+          respond(:record_not_found, 404, exception.to_s)
         end
-        # rescue_from CustomError do |e|
-        #   byebug
-        #   respond(e.error, e.status, e.message.to_s)
-        # end
-        # rescue_from StandardError do |e|
-        #   # byebug
-        #   respond(:standard_error, 500, e.to_s)
-        # end
+        rescue_from CustomError do |exception|
+          byebug
+          respond(exception.error, exception.status, exception.message.to_s)
+        end
+        rescue_from StandardError do |exception|
+          respond(:standard_error, 500, exception.to_s)
+        end
       end
     end
 
