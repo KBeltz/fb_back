e370fae00cbdc3e00a06db4a26a798a31c770b94
Andrew Lodes <andrew.lodes@atssecured.com>
2016-10-25 16:34:53 -0500

    FB-37 serializer changes for belongs_to

--- app/serializers/form_serializer.rb
+++ app/serializers/form_serializer.rb
@@ -2,7 +2,7 @@ class FormSerializer < ActiveModel::Serializer
   attributes :id, :update_by, :update_time, :insert_by, :insert_time, :date_filled_out, :is_complete
 
   has_one :template
-  # belongs_to :user, :foreign_key => "recipient_user_id", :class_name => 'User'
-  has_one :user
+  belongs_to :user, :foreign_key => "recipient_user_id", :class_name => 'User'
+  # has_one :user
   has_many :submissions
 end
