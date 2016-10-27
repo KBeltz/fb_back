223efd0a58497b0c0db5743e808db2d271f63f87
skyler.sidner <skyler.sidner@Skylers-MacBook-Pro.local>
2016-10-27 14:02:40 -0500

    fixed serializer to now save templateFields

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -15,19 +15,10 @@ class TemplatesController < ApplicationController
 
   # POST /templates
   def create
-    puts "template_params: " + template_params.to_s
-    # user = User.find_by(user_id: 27)
     @template = Template.new(template_params)
-    # if template_params['insert_by_id']
-    #   user = User.find_by(id: 27)
-    # end
-    # puts "user: " + user.to_s
-    # @template.insert_by_id = user
-    # puts "@template.insert_by: " + @template.insert_by.to_s
     if @template.save
       render json: @template, status: :created, location: @template
     else
-      puts "Errors: " + @template.errors.full_messages.first
       render json: @template.errors, status: :unprocessable_entity
     end
   end
@@ -54,9 +45,6 @@ class TemplatesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def template_params
-      # params.require(:template).permit(:attributes, :relationships, :data, :type, :template_name, :insert_by, :update_by, :update_time, :insert_time)
       ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:template_name, :insert_by, :update_by, :update_time, :insert_time])
-      # ActiveModelSerializers::Deserialization.jsonapi_parse!(params)
-      # {"data"=>{"attributes"=>{"template-name"=>"This is a name!"}, "relationships"=>{"user"=>{"data"=>{"type"=>"users", "id"=>"27"}}}, "type"=>"templates"}}
     end
 end
